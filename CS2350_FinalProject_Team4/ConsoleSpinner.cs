2918e1804a9d2c9f42073d87a8fabbfff2453065
Rick Bradley <rick.bradley.43@gmail.com>
2013-04-16 18:24:15 -0400

    Starting work on mainscreen animations

---
+++ CS2350_FinalProject_Team4/ConsoleSpinner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace CS2350_FinalProject_Team4
+{
+    class ConsoleSpinner
+    {
+    int counter;
+    public ConsoleSpinner()
+    {
+        counter = 0;
+    }
+    public void Turn()
+    {
+        counter++;        
+        switch (counter % 2)
+        {
+            case 0:
+                Console.WriteLine("  **    **  ");
+                Console.WriteLine(" ****  **** ");
+                Console.WriteLine("************");
+                Console.WriteLine(" ********** ");
+                Console.WriteLine("  ********  ");
+                Console.WriteLine("   ******   ");
+                Console.WriteLine("    ****    ");
+                Console.WriteLine("     **     ");
+                break;
+            case 1: 
+                Console.WriteLine("  !!    !!  ");
+                Console.WriteLine(" !!!!  !!!! ");
+                Console.WriteLine("!!!!!!!!!!!!");
+                Console.WriteLine(" !!!!!!!!!! ");
+                Console.WriteLine("  !!!!!!!!  ");
+                Console.WriteLine("   !!!!!!   ");
+                Console.WriteLine("    !!!!    ");
+                Console.WriteLine("     !!     ");
+                break;
+        }
+        Thread.Sleep(100);
+        Console.Clear();
+
+
+
+    }
+
+
+    }
+}
+/*
+
+  **    **  
+ ****  **** 
+************
+ ********** 
+  ********  
+   ******   
+    ****    
+     **     
+*/
--- CS2350_FinalProject_Team4/MainConsole.cs
+++ CS2350_FinalProject_Team4/MainConsole.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace CS2350_FinalProject_Team4
 {
@@ -9,8 +10,33 @@ namespace CS2350_FinalProject_Team4
     {
         static void Main(string[] args)
         {
-            Sound.RangedBeep();
-            Sound.SystemSound();
+
+            //alter console graphics and measurements
+          Console.Title = "Richard_Bradley_CS2350_Lab_7 Assignment 7";
+            Console.SetWindowSize(85, 43);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.CursorVisible = false;
+            Console.Clear();
+            /* 
+                       Sound.RangedBeep();
+                       Sound.SystemSound();
+
+                       Console.Write("Loading");
+                       for (int i = 0; i < 10; i++)
+                       {
+                           Thread.Sleep(100);
+                           Console.Write(".");
+                       }
+             */
+
+            ConsoleSpinner spin = new ConsoleSpinner();
+            Console.Write("Working....");
+            while (true)
+            {
+                spin.Turn();
+            }
         }
+
     }
 }
