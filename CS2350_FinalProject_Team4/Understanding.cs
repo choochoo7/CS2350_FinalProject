59b95b845ce8661145aaf143e21dd4b41ec62813
stan002 <stan002@ymail.com>
2013-04-20 18:30:31 -0300

    Change the question.

--- CS2350_FinalProject_Team4/Understanding.cs
+++ CS2350_FinalProject_Team4/Understanding.cs
@@ -36,8 +36,11 @@ namespace CS2350_FinalProject_Team4
 
             Graphics.UnderstandFont();
 
-            Console.SetCursorPosition(20, 22);
-            Console.Write("Are you Understanding? Enter 1 for No, or 2 for Yes\n");
+            Console.SetCursorPosition(0, 22);
+            Console.Write("Your best friend confesses that he/she borrowed your car to run to the grocery store for" +
+                " an emergency ice cream run. Someone keyed the car while he/she was there. " +
+                "Do you (1) flip out and find a new best friend or (2) try calm down and empathize;"+
+                " ice cream does sound pretty good.\n");
             Console.SetCursorPosition(35, 27);
             inValue = Console.ReadLine();
             //send input to error check before assigning
